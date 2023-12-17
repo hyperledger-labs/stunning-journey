7a5ab2329a1b61f6bece7832e0489e3fdce90a08
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-12-17 22:09:20 +0100

    Fix compat: Use ethereum address for off-chain id
    - Currently, the names "Alice" and "Bob" are used as off-chain
      addresses.
    
    - However, as per go-perun v0.9.0, off-chain id should be a valid
      ethereum address.
    
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- cortex-m-demo/src/application.rs
+++ cortex-m-demo/src/application.rs
@@ -53,7 +53,7 @@ pub struct Config {
     pub other_participant: (IpAddress, u16),
     pub service_server: (IpAddress, u16),
     pub listen_port: u16,
-    pub participants: [&'static str; 2],
+    pub participants: [&'static [u8; 20]; 2],
 }
 
 /// State machine for the demo logic: Fetch information about the blockchain
@@ -312,9 +312,9 @@ where
             None => return Err(Error::InvalidState),
         }
 
-        let my_wire_address = self.config.participants[0].into();
+        let my_wire_address = self.config.participants[0].to_vec();
 
-        if env.recipient[..] != self.config.participants[0].as_bytes()[..] {
+        if env.recipient[..] != my_wire_address {
             return Err(Error::UnexpectedMsg);
         }
 
@@ -428,7 +428,7 @@ where
             let peers: Vec<Vec<u8>> = self
                 .config
                 .participants
-                .map(|p| p.as_bytes().to_vec())
+                .map(|p| p.to_vec())
                 .into();
             self.client.send_handshake_msg(&peers[0], &peers[1]);
 
@@ -588,7 +588,7 @@ where
         let peers = self
             .config
             .participants
-            .map(|p| p.as_bytes().to_vec())
+            .map(|p| p.to_vec())
             .into();
         let prop = LedgerChannelProposal {
             proposal_id: self.rng.gen(),
