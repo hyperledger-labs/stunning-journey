manoranjith/fork-perun-rs

Replace the older watcher messages with newer one
- Replace the older definitions of watcher messages with newer ones.

- Unlike the previous watcher messages, there is no start dispute or
  dispute notification. Instead, the register and withdraw of the
  funding service are used to close a channel.

Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

src/messages.rs
@@ -6,7 +6,7 @@ mod watch_request;
 pub use funding_request::{LedgerChannelFundingRequest, RegisterReq, WithdrawReq, AdjudicatorReq, Transaction};
 pub use proposal::{LedgerChannelProposal, LedgerChannelProposalAcc};
 pub use update::{LedgerChannelUpdate, LedgerChannelUpdateAccepted};
-pub use watch_request::{SignedWithdrawalAuth, WatchInfo};
+pub use watch_request::{SignedWithdrawalAuth, WatchInfo, StartWatchingLedgerChannelReq};
 
 use crate::abiencode::types::Hash;
 use alloc::string::String;
@@ -25,12 +25,7 @@ pub enum ConversionError {
 pub enum WatcherRequestMessage {
     /// Ask the Watcher to start watching the blockchain for disputes.
     /// Acknowledged with [WatcherReplyMessage::Ack] containing `version == 0`.
-    WatchRequest(WatchInfo),
-    /// Ask the Watcher to initialize a dispute on-chain, with the given state.
-    /// It currently does not contain the parameters for reducing the amount of
-    /// communication needed. Adding it might be useful to make the watcher less
-    /// stateful.
-    StartDispute(WatchInfo),
+    WatchRequest(StartWatchingLedgerChannelReq),
 }
 
 /// Messages sent from the Watcher service.

hyperledger-labs/stunning-journey

src/messages.rs
@@ -8,7 +8,7 @@ pub use funding_request::{
 };
 pub use proposal::{LedgerChannelProposal, LedgerChannelProposalAcc};
 pub use update::{LedgerChannelUpdate, LedgerChannelUpdateAccepted};
-pub use watch_request::{SignedWithdrawalAuth, WatchInfo};
+pub use watch_request::{SignedWithdrawalAuth, StartWatchingLedgerChannelReq, WatchInfo};
 
 use crate::abiencode::types::Hash;
 use alloc::string::String;
@@ -27,12 +27,7 @@ pub enum ConversionError {
 pub enum WatcherRequestMessage {
     /// Ask the Watcher to start watching the blockchain for disputes.
     /// Acknowledged with [WatcherReplyMessage::Ack] containing `version == 0`.
-    WatchRequest(WatchInfo),
-    /// Ask the Watcher to initialize a dispute on-chain, with the given state.
-    /// It currently does not contain the parameters for reducing the amount of
-    /// communication needed. Adding it might be useful to make the watcher less
-    /// stateful.
-    StartDispute(WatchInfo),
+    WatchRequest(StartWatchingLedgerChannelReq),
 }
 
 /// Messages sent from the Watcher service.